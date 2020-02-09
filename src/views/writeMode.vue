7393a47c011cd04c3581021bab16482b6eacf3e1
Shun-Maron <kurisyun4321@gmail.com>
2020-02-09 13:16:48 +0900

    add: 文章入力エリアとタイマーエリアの分割

--- src/views/writeMode.vue
+++ src/views/writeMode.vue
@@ -1,10 +1,48 @@
 <template>
-    <h1>スマホ、PCで書きながら時間測定。</h1>
+  <div>
+    <!-- <h1>スマホ、PCで書きながら時間測定。</h1> -->
+    <!-- <div class="container"> -->
+    <div class="row">
+      <div class="col-9">
+        <div class="row">
+          <div class="col">
+            <b-form-textarea
+              id="textarea"
+              v-model="text"
+              placeholder="Enter something..."
+              rows="3"
+              max-rows="6"
+            ></b-form-textarea>
+
+            <pre class="mt-3 mb-0">{{ text }}</pre>
+          </div>
+        </div>
+      </div>
+      <div class="col-3 sideArea">ピンクのサイドバー</div>
+    </div>
+  </div>
+  <!-- </div> -->
 </template>
 
 <script>
 export default {
-    name: 'writeMode',
-    // components: {}
-}
+  name: "writeMode",
+   data() {
+      return {
+        text: ''
+      }
+    }
+  // components: {}
+};
 </script>
+
+<style scoped>
+div {
+  border: 1px solid;
+}
+
+.sideArea {
+  min-height: 70vh;
+  max-height: 75vh;
+}
+</style>
