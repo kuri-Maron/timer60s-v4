adbdb51f43fad57fb4680678270d83c054849334
Shun-Maron <kurisyun4321@gmail.com>
2020-02-09 17:49:41 +0900

    add: タイマー未完成版

--- src/views/writeMode.vue
+++ src/views/writeMode.vue
@@ -11,38 +11,72 @@
               v-model="text"
               placeholder="Enter something..."
               rows="3"
-              max-rows="6"
+              max-rows="5"
             ></b-form-textarea>
-
             <pre class="mt-3 mb-0">{{ text }}</pre>
           </div>
         </div>
       </div>
-      <div class="col-3 sideArea">ピンクのサイドバー</div>
+      <div class="col-3 sideArea">
+        <!-- <p>ピンクのサイドバー</p> -->
+        <b-button class="rounded-circle minitimer" variant="outline-warning">
+          <vac :left-time="60000" :auto-start="false">
+            <template v-slot:before>
+              <!-- boforeの段階から変数とれんのか？ -->
+              <span>60</span>
+            </template>
+            <template v-slot:process="{ timeObj }">
+              <span>{{timeObj.s}}</span>
+            </template>
+            <template v-slot:finish>
+              <span>Done!</span>
+            </template>
+          </vac>
+        </b-button>
+      </div>
     </div>
   </div>
   <!-- </div> -->
 </template>
 
 <script>
+import Vue from "vue";
+import vueAwesomeCountdown from "vue-awesome-countdown";
+
+Vue.use(vueAwesomeCountdown, "vac");
+
 export default {
   name: "writeMode",
-   data() {
-      return {
-        text: ''
-      }
-    }
+  data() {
+    return {
+      text: ""
+    };
+  }
   // components: {}
 };
 </script>
 
 <style scoped>
 div {
-  border: 1px solid;
+  /* border: 1px solid; */
+}
+
+pre {
+  color: azure;
 }
 
 .sideArea {
   min-height: 70vh;
   max-height: 75vh;
+}
+
+.minitimer {
+  font-size: 10vmin;
+}
+
+#textarea {
+    background-color: #444;
+    color: azure;
+    border :1px solid #42b983;
 }
 </style>
